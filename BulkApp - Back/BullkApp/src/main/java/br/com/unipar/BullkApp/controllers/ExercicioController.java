ecda311b964eebf1b2dbd36ece2657c931796ba6
Matheus Oliveira Pereira <matheus.pereira.00@edu.unipar.br>
2023-07-26 12:43:47 -0300

    Implementação de service e controller referente a modelo Exercicio

---
+++ BulkApp - Back/BullkApp/src/main/java/br/com/unipar/BullkApp/controllers/ExercicioController.java
@@ -0,0 +1,49 @@
+package br.com.unipar.BullkApp.controllers;
+
+import br.com.unipar.BullkApp.model.Exercicio;
+import br.com.unipar.BullkApp.services.ExercicioService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/exercicio")
+@Api(description = "Controlador REST Responsável pela Operações que representam o objeto de negócios Exercicio")
+public class ExercicioController {
+    @Autowired
+    private ExercicioService exercicioService;
+
+    @PostMapping
+    @ApiOperation(value = "Operação resposável pela Inserção de um novo Exercicio")
+    public Exercicio insert(@RequestBody @Valid Exercicio exercicio) throws Exception{
+        return exercicioService.insert(exercicio);
+    }
+
+    @PutMapping
+    @ApiOperation(value = "Operação responsável pela Atualização de um Exercicio já existente")
+    public Exercicio update(@RequestBody @Valid Exercicio exercicio) throws Exception{
+        return exercicioService.update(exercicio);
+    }
+
+    @GetMapping(path = "/{id}")
+    @ApiOperation(value = "Operação resposável pela busca de Exercicio via ID")
+    public Exercicio findById(@PathVariable Long id) throws Exception{
+        return exercicioService.findById(id);
+    }
+
+    @GetMapping(path = "/filter")
+    @ApiOperation(value = "Operação responsável pela busca do Exercicio via Nome")
+    public List<Exercicio> findByFilters(@RequestParam("descricao") String descricao)throws Exception{
+        return exercicioService.findByFilters(descricao);
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Operação resposável por listar todos os Exercicios cadastrados no sistema")
+    public List<Exercicio> findAll() throws Exception{
+        return exercicioService.findAll();
+    }
+}
---
+++ BulkApp - Back/BullkApp/src/main/java/br/com/unipar/BullkApp/services/ExercicioService.java
@@ -0,0 +1,60 @@
+package br.com.unipar.BullkApp.services;
+
+import br.com.unipar.BullkApp.model.Exercicio;
+import br.com.unipar.BullkApp.model.Usuario;
+import br.com.unipar.BullkApp.repositories.ExercicioRepository;
+import io.swagger.annotations.ApiModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@ApiModel(description = "Classe responsável pela regras de Negócio referente ao Exercicio")
+public class ExercicioService {
+
+    @Autowired
+    private ExercicioRepository exercicioRepository;
+
+    public Exercicio insert(Exercicio exercicio) throws Exception{
+        exercicioRepository.saveAndFlush(exercicio);
+        return exercicio;
+    }
+
+    public Exercicio update(Exercicio exercicio) throws Exception {
+        validaUpdate(exercicio);
+        exercicioRepository.saveAndFlush(exercicio);
+        return exercicio;
+    }
+
+    public Exercicio findById(Long id) throws Exception{
+        Optional<Exercicio> retorno = exercicioRepository.findById(id);
+        if (retorno.isPresent()){
+            return retorno.get();
+        }
+        else {
+            throw new Exception("Exercicio " + id + " não encontrado");
+        }
+    }
+
+    public List<Exercicio> findByFilters(String nome) throws Exception{
+        return exercicioRepository.findByDescricaoContainingAllIgnoringCase(nome);
+    }
+
+    public List<Exercicio> findAll() throws Exception{
+        return exercicioRepository.findAll();
+    }
+
+    private void validaInsert(Usuario usuario) throws Exception{
+        if (usuario.getId() != null){
+            throw new Exception("Não é necessário informar o ID para inserir um novo Exercicio");
+        }
+    }
+
+    private void validaUpdate(Exercicio exercicio) throws Exception{
+        if (exercicio.getId() == null){
+            throw new Exception("É necessário informar o ID para atualizar o cadastro do Exercicio");
+        }
+    }
+}
