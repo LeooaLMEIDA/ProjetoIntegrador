23b40caae0f184f848cb8ca81387f683c5980d43
Leonardo Almeida <leonardo.l@edu.unipar.br>
2023-08-05 11:51:04 -0300

    começo projeto web

---
+++ bullkapp-web/bullkapp_web/lib/app_widget.dart
@@ -0,0 +1,19 @@
+import 'package:bullkapp_web/theme/bullkTheme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'login.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "BullkApp",
+      theme: myTheme,
+      home: const LoginScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ bullkapp-web/bullkapp_web/lib/theme/bullkTheme.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: file_names, prefer_const_constructors
+import 'package:flutter/material.dart';
+
+ThemeData myTheme = ThemeData(
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Color.fromARGB(250, 1, 30, 62),
+  ),
+  primaryColor: Color.fromARGB(248, 1, 30, 62),
+);
