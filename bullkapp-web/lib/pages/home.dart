c8367ecc387eec2017634d56060578b673150345
Leonardo Almeida <leonardo.l@edu.unipar.br>
2023-08-14 22:12:57 -0300

    Teste da Tela Home e Inclusão de Fonts no Projeto.

--- bullkapp-web/lib/pages/home.dart
+++ bullkapp-web/lib/pages/home.dart
@@ -1,5 +1,4 @@
 import 'package:bullkappweb/components/appbar.dart';
-import 'package:bullkappweb/data/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -23,66 +22,74 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: const CustomAppBar(
         title: "",
       ),
-      body: Stack(
+      body: Row(
         children: [
-          Row(
-            children: [
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                width: _drawerOn ? 300 : 60,
-                color: const Color.fromARGB(255, 4, 53, 101),
+          AnimatedContainer(
+            duration: const Duration(
+              milliseconds: 300,
+            ),
+            width: _drawerOn ? 300 : 60,
+            color: const Color.fromARGB(255, 4, 53, 101),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 16.0,
+                        left: 16.0,
+                      ),
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            child: Image.asset(
+                              "assets/images/iconbar.png",
+                              width: 20,
+                            ),
+                            onTap: () {
+                              setState(
+                                () {
+                                  _drawerOn = !_drawerOn;
+                                },
+                              );
+                            },
+                          ),
+                          if (_drawerOn)
+                            const Padding(
+                              padding: EdgeInsets.only(
+                                left: 24.0,
+                              ),
+                              child: Text(
+                                "Menu",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "Voltaire",
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16.0),
                 child: Column(
                   children: [
-                    Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(
-                            top: 16.0,
-                            left: 16.0,
-                          ),
-                          child: Row(
-                            children: [
-                              GestureDetector(
-                                child: Image.asset(
-                                  "assets/images/iconbar.png",
-                                  width: 20,
-                                ),
-                                onTap: () {
-                                  setState(
-                                    () {
-                                      _drawerOn = !_drawerOn;
-                                    },
-                                  );
-                                },
-                              ),
-                              if (_drawerOn)
-                                const Padding(
-                                  padding: EdgeInsets.only(
-                                    left: 24.0,
-                                  ),
-                                  child: Text(
-                                    "Menu",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontFamily: "Voltaire",
-                                      fontSize: 16,
-                                    ),
-                                  ),
-                                ),
-                            ],
-                          ),
-                        ),
-                      ],
+                    Image.asset(
+                      "assets/images/Logo2.png",
+                      width: 280,
                     ),
                   ],
                 ),
               ),
-              Expanded(
-                child: Center(
-                  child: Image.asset("assets/images/Logo2.png"),
-                ),
-              ),
-            ],
+            ),
           ),
         ],
       ),
