0ac2e82419c5b08da231a362b8db64b0eeb452f4
Leonardo Almeida <leonardo.l@edu.unipar.br>
2023-08-27 21:27:46 -0300

    Alteração nas Constantes

--- bullkapp-front/lib/data/constants.dart
+++ bullkapp-front/lib/data/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart' show Color;
 
 const String apiBaseURL = "https://99ec-186-194-148-136.ngrok-free.app";
 const Color defaultColor = Color.fromARGB(250, 1, 30, 62);
+const String defaultImage = "images/Avatar.png";
